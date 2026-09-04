c594b23d43e160b6ac4ecf37ab59c1b3c2125291
Matin <ali_matin16@yahoo.com>
2026-09-04 19:31:54 +0330

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "CICD FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+
+    
